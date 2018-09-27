1c63334b700f973a7a7bbe6c75b0f954840c3583
v2hero <wf10518590@gmail.com>
2018-09-27 17:11:48 +0800

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,18 @@
-cd /mybin
-echo -e "$CONFIG_JSON" > config.json
-if [ "$CERT_PEM" != "$KEY_PEM" ]; then
-  echo -e "$CERT_PEM" > cert.pem
-  echo -e "$KEY_PEM"  > key.pem
-fi
-./cgi
+#cd /v2raybin
+#echo -e "$CONFIG_JSON" > config.json
+#if [ "$CERT_PEM" != "$KEY_PEM" ]; then
+#  echo -e "$CERT_PEM" > cert.pem
+#  echo -e "$KEY_PEM"  > key.pem
+#fi
+
+cd /v2ray
+wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
+unzip v2ray.zip 
+mv /v2ray/v2ray-v$VER-linux-64/v2ray .
+mv /v2ray/v2ray-v$VER-linux-64/v2ctl .
+mv /v2ray/v2ray-v$VER-linux-64/geoip.dat .
+mv /v2ray/v2ray-v$VER-linux-64/geosite.dat .
+
+chmod +x v2ray v2ctl
+sed -i "s/your_uuid/$UUID/g" config.json
+./v2ray
